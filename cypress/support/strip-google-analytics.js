70e1407b19e4f834f85300d73ba66dbe52859e08
Koen Verheyen <koen@inuits.eu>
2017-10-03 09:19:53 +0200

    Improved strip Google Analytics script (also works when testing different env)

--- cypress/support/strip-google-analytics.js
+++ cypress/support/strip-google-analytics.js
@@ -1,26 +1,40 @@
+let gaScriptStripped = false;
+
 Cypress.on('window:before:load', function(win) {
     let createElement = win.document.createElement;
+
     win.document.createElement = function(tag) {
         if (tag === 'script' && win.document.currentScript.innerHTML.includes('google-analytics.com')) {
             tag = 'custom_script';
 
+            gaScriptStripped = true;
+
             console.log('Removed Google analytics script');
         }
+
         return createElement.call(win.document, tag);
     }
 });
 
-it('should never load the google analytics script', function() {
-    cy.visit('/');
+describe('The test runner', function() {
+    it('should never load the google analytics script', function() {
+        cy.visit('/');
 
-    cy.get('script').each(function(script)  {
-        cy.wrap(script)
-            .invoke('prop', 'src')
-            .should('not.contain', 'google-analytics.com');
+        cy.get('script').each(function(script)  {
+            cy.wrap(script)
+                .invoke('prop', 'src')
+                .should('not.contain', 'google-analytics.com');
+        });
+
+        cy.get('custom_script')
+            .should(function($cs) {
+                if (gaScriptStripped) {
+                    expect($cs).to.have.length(1);
+
+                    expect($cs.prop('src')).to.contain('google-analytics.com')
+                } else {
+                    expect($cs).to.not.exist;
+                }
+            })
     });
-
-    cy.get('custom_script')
-        .should('have.length', 1)
-        .invoke('prop', 'src')
-        .should('contain', 'google-analytics.com');
 });
