cd52b8b8e9f51099408953c415341da08e837383
Koen Verheyen <koen.verheyen@inuits.eu>
2017-10-04 17:31:48 +0200

    Databases page tests

--- cypress/integration/databases/databases.spec.js
+++ cypress/integration/databases/databases.spec.js
@@ -1,15 +1,23 @@
-describe('databases page', function() {
-    xit('WOS should be on this page', function() {
-    
-// test "web of science"
-    
+describe('The databases page', function() {
+    beforeEach(function() {
+        cy.visit('/databases');
     });
 
-    xit('page should link to google', function() {
-    
-// test "scholar.google.com"
-    
+    it('should contain a link to WOS', function() {
+        cy.get('.link-external')
+            .contains('Web of Science')
+            .should('have.length', 1)
+            .should('be.visible')
+            .invoke('prop', 'href')
+            .should('contain', 'webofknowledge.com/WOS');
     });
 
-
+    it('should link to google scholar', function() {
+        cy.get('.link-external')
+            .contains('Google Scholar')
+            .should('have.length', 1)
+            .should('be.visible')
+            .invoke('prop', 'href')
+            .should('contain', 'scholar.google.com');
+    });
 });
