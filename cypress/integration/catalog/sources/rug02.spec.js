6bfed6cd1dc1d8fa27743a45a5f76928fd6e00dc
Koen Verheyen <koen.verheyen@inuits.eu>
2017-10-04 15:23:08 +0200

    Rug02 tests

--- cypress/integration/catalog/sources/rug02.spec.js
+++ cypress/integration/catalog/sources/rug02.spec.js
@@ -1,11 +1,13 @@
 describe('Data source rug02', function() {
-    xit('should have more than 500K hits', function() {
-        cy.request({
-                url: 'http://lib.ugent.be/catalog/source:rug02',
-                followRedirect: true,
-            })
-            .then((resp) => {
+    it('should have more than 500K hits', function() {
+        cy.visit('/catalog/source:rug02');
+
+        cy.get('.search-result-count > strong:eq(2)')
+            .invoke('prop', 'innerText')
+            .should(function(results) {
+                results = parseInt(results.replace(/,/g, ''));
+
+                expect(results).to.be.greaterThan(500000);
             });
     });
-
 });
