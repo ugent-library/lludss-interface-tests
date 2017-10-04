dce643046082e21126a62b9c0265c06927b04820
Koen Verheyen <koen.verheyen@inuits.eu>
2017-10-04 17:31:35 +0200

    Info/About page tests

--- cypress/integration/info/info.spec.js
+++ cypress/integration/info/info.spec.js
@@ -1,18 +1,42 @@
-describe('info/about pages', function() {
-    xit('should contain a list of links to /info', function() {
-    
-// messages can be found at http://widgets.lib.ugent.be/list_messages/web_info_en.json for English
-// messages can be found at http://widgets.lib.ugent.be/list_messages/web_info_nl.json for Dutch
-// messages have a "code": "en_registration" or "registration_en" that translates to the url "/en/info/registration"
-    
-    
+describe('The info/about page', function() {
+    ['en', 'nl'].forEach(function(lang) {
+        it(`should contain a list of links to /${lang}/info`, function() {
+            cy.visit(`/${lang}/info`);
+
+            cy.request(`http://widgets.lib.ugent.be/list_messages/web_info_${lang}.json`)
+                .then(function(response) {
+                    cy.get('.section-title:eq(0) ~ ul > li > a').as('links')
+                        .should('have.length', response.body.messages_total);
+
+                    cy.get('@links')
+                        .each(function($a, index) {
+                            let message = response.body.messages[index];
+                            let code = message.code.replace(`${lang}_`, '').replace(`_${lang}`, '');
+
+                            cy.wrap($a)
+                                .should('have.text', message.title)
+                                .should('have.attr', 'href')
+                                .should('match', new RegExp(`/${lang}/info/${code}`));
+                        });
+                });
+        });
     });
 
-    xit('page should link to onderzoektips', function() {
-    
-// test "onderzoektips.ugent.be"
-    
-    });
+    it('should link to research tips', function() {
+        cy.visit('/info');
 
+        cy.get('.section-title:eq(1) ~ ul > li > a')
+            .should('be.visible')
+            .each(function($a) {
+                cy.request({
+                    url: $a.prop('href'),
+                    followRedirect: false,
+                })
+                    .then(function(resp) {
+                        expect(resp.status).to.eq(302);
+                        expect(resp.redirectedToUrl).to.contain('onderzoektips.ugent.be');
+                    });
+            });
+    });
 });
 
