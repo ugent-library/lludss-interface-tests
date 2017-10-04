61b8d17fb7262efb607491786be38c48fcd23179
Koen Verheyen <koen.verheyen@inuits.eu>
2017-10-04 15:23:27 +0200

    Catalog type periodical tests

--- cypress/integration/catalog/types/periodical.spec.js
+++ cypress/integration/catalog/types/periodical.spec.js
@@ -1,11 +1,13 @@
 describe('Type periodical', function() {
-    xit('should have more than 1OOK hits', function() {
-        cy.request({
-                url: 'http://lib.ugent.be/catalog/type:periodical',
-                followRedirect: true,
-            })
-            .then((resp) => {
+    it('should have more than 1OOK hits', function() {
+        cy.visit('/catalog/type:periodical');
+
+        cy.get('.search-result-count > strong:eq(2)')
+            .invoke('prop', 'innerText')
+            .should(function(results) {
+                results = parseInt(results.replace(/,/g, ''));
+
+                expect(results).to.be.greaterThan(100000);
             });
     });
-
 });
