f446ecfc89dfa57f93f99beb2a3dbd2877f8a2ae
Koen Verheyen <koen.verheyen@inuits.eu>
2017-10-09 16:10:52 +0200

    Added tests for 404 pages (invalid routes/deleted records)

---
+++ cypress/integration/404.spec.js
@@ -0,0 +1,29 @@
+describe('The 404 page', function() {
+    it('should return a 404 HTTP status code for non existent pages', function() {
+        cy.request(
+            {
+                url: '/non-existing-page',
+                failOnStatusCode: false,
+            })
+            .its('status')
+            .should('eq', 404);
+    });
+
+    it('should return a 404 HTTP status code for deleted records', function() {
+        cy.request(
+            {
+                url: '/catalog/rug01:001223181',
+                failOnStatusCode: false,
+            })
+            .its('status')
+            .should('eq', 404);
+
+        cy.request(
+            {
+                url: '/catalog/rug01:001223181.json',
+                failOnStatusCode: false,
+            })
+            .its('status')
+            .should('eq', 404);
+    });
+});
