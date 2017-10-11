40aa458f70a4c0f6642d86ef8aa59a3ad27b4195
Koen Verheyen <koen.verheyen@inuits.eu>
2017-10-11 16:37:19 +0200

    Tests for catalog sorting

--- cypress/support/commands/get-query-parameter.js
+++ cypress/support/commands/get-query-parameter.js
@@ -1,6 +1,21 @@
-Cypress.Commands.add('getQueryParameter', (name) => {
-    return cy.location('search', {log: false})
+Cypress.Commands.add('getQueryParameter', {prevSubject: 'optional'}, (subject, name, _default = null) => {
+    let subj = null;
+    let url = null;
+
+    if (Cypress._.isNil(subject)) {
+        subj = cy.location('search', {log: false});
+    } else if (Cypress._.isObject(subject) && subject.hasOwnProperty('toString')) {
+        subj = cy.wrap(subject.toString(), {log: false});
+    } else if (Cypress._.isString(subject)) {
+        subj = cy.wrap(subject, {log: false});
+    } else {
+        throw new Error(`Cannot get query parameter for ${subject}`);
+    }
+
+    return subj
         .then((search) => {
+            url = search;
+
             let hashes = search.slice(search.indexOf('?') + 1).split('&');
             return hashes.reduce((params, hash) => {
                 let [key, val] = hash.split('=');
@@ -8,15 +23,26 @@ Cypress.Commands.add('getQueryParameter', (name) => {
             }, {});
         })
         .then(function(query) {
-            let result = query[name];
+            let result = _default;
+            let message = [name];
+
+            if (name in query) {
+                result = query[name];
+                message.push(result);
+            } else {
+                message.push(result);
+                message.push('(default)');
+            }
 
             Cypress.log({
                 name: 'getQueryParameter',
-                message: [name, result],
+                message: message,
                 consoleProps: () => {
                     return {
-                        name: name,
-                        result: result,
+                        'subject': url,
+                        'name': name,
+                        'result': result,
+                        'default': _default,
                     };
                 },
             });
