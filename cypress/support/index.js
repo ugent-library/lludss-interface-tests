809052314165b523b19966eb8d4e7e6758ef5053
Koen Verheyen <koen.verheyen@inuits.eu>
2017-12-28 11:08:05 +0100

    Extracted map command to cypress-common

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,9 +1,9 @@
 import './strip-google-analytics';
 
 import './common/helpers/chai';
+import './common/commands/map';
 
 import './commands/get-count';
 import './commands/get-query-parameter';
 import './commands/split';
-import './commands/map';
 import './commands/sum';
