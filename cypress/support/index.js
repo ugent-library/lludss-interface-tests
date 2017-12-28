1b9ef007396bd4546024cbb1b8ec414198eda26d
Koen Verheyen <koen.verheyen@inuits.eu>
2017-12-28 13:43:46 +0100

    Extracted random command to cypress-common

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -3,6 +3,7 @@ import './strip-google-analytics';
 import './common/helpers/chai';
 import './common/commands/map';
 import './common/commands/prop';
+import './common/commands/random';
 
 import './commands/get-count';
 import './commands/get-query-parameter';
