9b5cfdb516287252b3c1c29563aaab284aac40f0
Koen Verheyen <koen.verheyen@inuits.eu>
2017-12-28 15:56:14 +0100

    Extracted split command to cypress-common

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -5,7 +5,7 @@ import './common/commands/map';
 import './common/commands/prop';
 import './common/commands/random';
 import './common/commands/get-query-parameter';
+import './common/commands/split';
 
 import './commands/get-count';
-import './commands/split';
 import './commands/sum';
