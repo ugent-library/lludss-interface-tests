bd6903a0a352a2adf6a892c06c6edb16f53ea118
Koen Verheyen <koen.verheyen@inuits.eu>
2018-02-09 15:45:36 +0100

    Fixed open data test

--- cypress/integration/catalog/opendata.spec.js
+++ cypress/integration/catalog/opendata.spec.js
@@ -1,12 +1,18 @@
 describe('The Open Data features', function() {
     it('should have 6 working hyperlinks in the developers section', function() {
+        cy.server();
+        cy.route('/status/**', {}).as('ajax');
+
         let baseUrl = Cypress.config('baseUrl');
 
         cy.visit('/catalog/rug01:000763774');
 
+        cy.wait('@ajax');
+
         cy.get('#developers dl dd a').as('links')
             .should('exist')
-            .should('not.be.visible');
+            .should('not.be.visible')
+            .should('have.length', 6);
 
         cy.contains('a', 'For developers').click();
 
