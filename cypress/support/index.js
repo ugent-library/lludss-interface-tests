2e7671fd9f39e950bf4794aa961a354c3e7940ae
Koen Verheyen <koen.verheyen@inuits.eu>
2018-03-28 12:14:12 +0200

    Removed cypress-common as submodule and used yarn power to include it from GitHub

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,12 +2,12 @@ chai.use(require('chai-sorted'));
 
 import './strip-scripts';
 
-import './common/helpers/chai';
-import './common/commands/map';
-import './common/commands/prop';
-import './common/commands/random';
-import './common/commands/get-query-parameter';
-import './common/commands/split';
-import './common/commands/sum';
+import 'cypress-common/helpers/chai';
+import 'cypress-common/commands/map';
+import 'cypress-common/commands/prop';
+import 'cypress-common/commands/random';
+import 'cypress-common/commands/get-query-parameter';
+import 'cypress-common/commands/split';
+import 'cypress-common/commands/sum';
 
 import './commands/get-count';
