02c885117f8be4173a4896edbbdd248644549af2
Koen Verheyen <koen.verheyen@inuits.eu>
2018-05-17 10:24:16 +0200

    Fixes #1: Catalog card search tests

---
+++ cypress/integration/info/cards.spec.js
@@ -0,0 +1,55 @@
+describe('The digitized card catalogue', function() {
+    ['en', 'nl'].forEach(function(lang) {
+        describe(`In ${lang} language`, function() {
+            it('should load the catalog search form', function() {
+                cy.visit(`/${lang}/info/cards`);
+
+                cy.get('form:not(.navbar-form)')
+                    .should('have.length', 1)
+                    .should('have.attr', 'action')
+                    .should('contain', '/catalog/source:rug02-rug03');
+            });
+
+            it('should be able to search for cards', function() {
+                cy.get('form:not(.navbar-form)')
+                    .find('input[name=q]')
+                    .type('test{enter}');
+
+                cy.location('pathname')
+                    .should('end.with', '/catalog/source:rug02-rug03');
+
+                cy.getQueryParameter('q')
+                    .should('eq', 'test');
+            });
+
+            it('should be able to load a card detail page', function() {
+                cy.get('#documents .meta-title a')
+                    .random()
+                    .click({force: true});
+
+                cy.location('pathname')
+                    .should('match', /\/catalog\/rug0[23]:\d{9}$/);
+            
+                cy.getQueryParameter('q')
+                    .should('eq', 'test');
+
+                cy.getQueryParameter('source')
+                    .should('eq', 'rug02-rug03');
+            });
+
+            it('should be able to request a card', function() {
+                cy.go('back');
+
+                cy.get('#documents a.btn-primary')
+                .random().as('request-button')
+                .should('have.attr', 'href')
+                .should('match', /\/catalog\/rug0[23]:\d{9}\/requests\/new$/);
+
+                cy.get('@request-button').click();
+
+                cy.location('href')
+                    .should('end.with', '/user/signin');
+            });
+        });
+    });
+});
