5deac550c34c1663755d1cc021364d342b742cd1
Koen Verheyen <verheyen.koen@gmail.com>
2019-01-15 11:33:39 +0100

    Fixed pug01 test

--- cypress/integration/catalog/sources/pug01.spec.js
+++ cypress/integration/catalog/sources/pug01.spec.js
@@ -1,23 +1,24 @@
-describe('Data source pug01', function() {
-    it('should have more than 230K hits', function() {
+describe('Data source pug01', function () {
+  it('should have more than 230K hits', function () {
+    cy.visit('/catalog/source:pug01');
+
+    cy.get('.search-result-count > strong:eq(2)')
+      .getCount()
+      .should('be.greaterThan', 230000);
+  });
+
+  it('should have almost as many hits as biblio', function () {
+    cy.request('https://biblio.ugent.be/publication?limit=0&format=json')
+      .then((response) => {
+        const biblioTotal = response.body.total;
+
         cy.visit('/catalog/source:pug01');
 
+        // biblio wordt 's nachts gesynced met lib, dus er kan wel wat verschil zijn
         cy.get('.search-result-count > strong:eq(2)')
-            .getCount()
-            .should('be.greaterThan', 230000);
-    });
-
-    it('should have almost as many hits as biblio', function() {
-        cy.request('https://biblio.ugent.be/publication?limit=0&format=json')
-            .then((response) => {
-                const biblioTotal = response.body.total;
-
-                cy.visit('/catalog/source:pug01');
-
-                // biblio wordt 's nachts gesynced met lib, dus er kan wel wat verschil zijn
-                cy.get('.search-result-count > strong:eq(2)')
-                    .getCount()
-                    .should('be.within', biblioTotal - 5000, biblioTotal + 5000);
-            });
-    });
+          .getCount()
+          .should('be.greaterThan', 200000)
+          .should('be.within', biblioTotal - 15000, biblioTotal);
+      });
+  });
 });
