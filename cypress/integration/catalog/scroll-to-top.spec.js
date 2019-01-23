9c4800e792c54641fdcd3594f4ef88179ed173c7
Koen Verheyen <verheyen.koen@gmail.com>
2019-01-23 12:36:26 +0100

    Fixed/added scroll-to-top tests

--- cypress/integration/catalog/scroll-to-top.spec.js
+++ cypress/integration/catalog/scroll-to-top.spec.js
@@ -1,38 +1,92 @@
-describe('The scroll to top feature', function() {
-    it('should scroll to the top when clicked', function() {
-        cy.visit('/catalog');
+import { exec } from "child_process";
 
-        cy.window()
-            .then(function(w) {
-                cy.get('.js-backtotop').as('back-to-top')
-                    .should('not.be.inViewport', w);
+describe('The scroll to top feature', function () {
 
-                cy.scrollTo(0, 200, {duration: 100});
+    describe('should only be visible after scrolling', function () {
+        const executeTest = () => {
+            cy.visit('/catalog');
 
-                cy.get('@back-to-top').should('be.inViewport', w);
+            cy.window()
+                .then(function (w) {
+                    cy.get('.js-backtotop').as('back-to-top')
+                        .should('not.be.inViewport', w);
 
-                cy.scrollTo(0, '25%', {duration: 100});
+                    cy.scrollTo(0, 200, { duration: 100 });
 
-                cy.get('@back-to-top').should('be.inViewport', w);
+                    cy.get('@back-to-top').should('be.inViewport', w);
 
-                cy.scrollTo(0, '50%', {duration: 100});
+                    cy.scrollTo(0, '25%', { duration: 100 });
 
-                cy.get('@back-to-top').should('be.inViewport', w);
+                    cy.get('@back-to-top').should('be.inViewport', w);
 
-                cy.scrollTo(0, '75%', {duration: 100});
+                    cy.scrollTo(0, '50%', { duration: 100 });
 
-                cy.get('@back-to-top').should('be.inViewport', w);
+                    cy.get('@back-to-top').should('be.inViewport', w);
 
-                cy.scrollTo(0, '100%', {duration: 100});
+                    cy.scrollTo(0, '75%', { duration: 100 });
 
-                cy.get('@back-to-top').should('be.inViewport', w);
+                    cy.get('@back-to-top').should('be.inViewport', w);
 
-                // We need to scroll back to the top because after reloading, chrome would find the same position.
-                cy.scrollTo(0, 0, {duration: 100});
+                    cy.scrollTo(0, '100%', { duration: 100 });
 
-                cy.reload();
+                    cy.get('@back-to-top').should('be.inViewport', w);
 
-                cy.get('@back-to-top').should('not.be.inViewport', w);
-            });
+                    cy.reload();
+
+                    // Reload automatically reuses the scroll position
+                    cy.get('@back-to-top').should('be.inViewport', w);
+
+                    cy.scrollTo(0, 0, { duration: 100 });
+
+                    cy.get('@back-to-top')
+                        .should('not.be.visible');
+                });
+        }
+
+        it('on desktop', function () {
+            executeTest();
+        });
+
+        it('on mobile', function () {
+            cy.viewport('iphone-6');
+
+            executeTest();
+        });
+    });
+
+    describe('should scroll back to top when clicking', function () {
+        const executeTest = () => {
+            cy.visit('/catalog');
+
+            cy.window()
+                .then(function (w) {
+                    cy.scrollTo(0, '555', { duration: 100 });
+
+                    cy.wrap(Cypress.$(w))
+                        .invoke('scrollTop')
+                        .should('eq', 555);
+
+                    cy.get('.js-backtotop')
+                        .as('back-to-top')
+                        .click();
+
+                    cy.wrap(Cypress.$(w))
+                        .invoke('scrollTop')
+                        .should('eq', 0);
+
+                    cy.get('@back-to-top')
+                        .should('not.be.visible');
+                });
+        }
+
+        it('on desktop', function () {
+            executeTest();
+        });
+
+        it('on mobile', function () {
+            cy.viewport('iphone-6');
+
+            executeTest();
+        });
     });
 });
