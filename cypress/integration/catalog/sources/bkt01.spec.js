33766ab5d7974271f776d6825a0a0ab2c9be7b36
Koen Verheyen <verheyen.koen@gmail.com>
2019-02-08 12:22:20 +0100

    Second bkt01 test made no sense

--- cypress/integration/catalog/sources/bkt01.spec.js
+++ cypress/integration/catalog/sources/bkt01.spec.js
@@ -6,12 +6,4 @@ describe('Data source bkt01', function () {
       .getCount()
       .should('be.lessThan', 35000)
   })
-
-  it('should have fewer than 50K hits', function () {
-    cy.visit('/catalog/source:bkt01')
-
-    cy.get('.search-result-count > strong:eq(2)')
-      .getCount()
-      .should('be.lessThan', 50000)
-  })
 })
