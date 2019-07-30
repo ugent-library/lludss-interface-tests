0bb761202a040c42a72d9a2b5e77e74ab6a00183
Koen Verheyen <koen.verheyen@inuits.eu>
2019-07-30 13:17:49 +0200

    Added MECE tests for card catalogue

--- cypress/integration/catalog/types/mece.spec.js
+++ cypress/integration/catalog/types/mece.spec.js
@@ -1,18 +1,24 @@
-;['/catalog', '/catalog/source:rug01', '/catalog/source:pug01'].forEach(function(path) {
-  describe(`Catalog tests for path: ${path}`, function() {
-    before(function() {
+const paths = [
+  '/catalog',
+  '/catalog/source:rug01',
+  '/catalog/source:pug01',
+  '/catalog/source:rug02-rug03-rug04' // The card catalogue
+]
+
+paths.forEach(function (path) {
+  describe(`Catalog tests for path: ${path}`, function () {
+    before(function () {
       cy.visit(path)
     })
-
-    ;['Type', 'Language'].forEach(function(facet) {
-      describe(`The MECE ${facet} facet`, function() {
-        it('the sum of facet value counts should match total hits exactly', function() {
+    ;['Type', 'Language'].forEach(function (facet) {
+      describe(`The MECE ${facet} facet`, function () {
+        it('the sum of facet value counts should match total hits exactly', function () {
           cy.get('.search-result-count > strong:eq(2)')
             .getCount()
-            .then(function(totalResults) {
+            .then(function (totalResults) {
               cy.contains('.filters .form-group', facet)
                 .find('.checkbox label .mute .facet-count')
-                .map(function(facetCount) {
+                .map(function (facetCount) {
                   return parseInt(facetCount.innerText.replace(/,/g, ''))
                 })
                 .sum()
