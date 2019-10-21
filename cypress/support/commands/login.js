c2cacdd1178c910027f0b7337bb28748f4c784df
Koen Verheyen <koen.verheyen@inuits.eu>
2019-10-21 11:29:01 +0200

    Refactored login script (compact logging)

--- cypress/support/commands/login.js
+++ cypress/support/commands/login.js
@@ -1,9 +1,11 @@
 Cypress.Commands.add('login', () => {
-  cy.visit('/user/signin')
+  Cypress.log({ name: 'login', message: '***REMOVED***:***REMOVED***' })
 
-  cy.get('[data-target="#visitors"]').click()
+  cy.visit('/user/signin', { log: false })
 
-  cy.get('#username').type('***REMOVED***')
-  cy.get('#password').type('***REMOVED***')
-  cy.get(':submit[value="Sign in with visitors card"]').click()
+  cy.get('[data-target="#visitors"]', { log: false }).click({ log: false })
+
+  cy.get('#username', { log: false }).type('***REMOVED***', { log: false })
+  cy.get('#password', { log: false }).type('***REMOVED***', { log: false })
+  cy.get(':submit[value="Sign in with visitors card"]', { log: false }).click({ log: false })
 })
