eae5b72390bb14af02bef0644ba4f05f165eab47
Koen Verheyen <koen.verheyen@inuits.eu>
2020-01-10 15:47:08 +0100

    Use env variables for login command

--- cypress/support/commands/login.js
+++ cypress/support/commands/login.js
@@ -1,11 +1,11 @@
 Cypress.Commands.add('login', () => {
-  Cypress.log({ name: 'login', message: '***REMOVED***:***REMOVED***' })
+  Cypress.log({ name: 'login' })
 
   cy.visit('/user/signin', { log: false })
 
   cy.get('[data-target="#visitors"]', { log: false }).click({ log: false })
 
-  cy.get('#username', { log: false }).type('***REMOVED***', { log: false })
-  cy.get('#password', { log: false }).type('***REMOVED***', { log: false })
+  cy.get('#username', { log: false }).type(Cypress.env('login_username'), { log: false })
+  cy.get('#password', { log: false }).type(Cypress.env('login_password'), { log: false })
   cy.get(':submit[value="Sign in with visitors card"]', { log: false }).click({ log: false })
 })
