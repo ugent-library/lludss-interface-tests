bb5f25e4dc9b83ac5045a1372b0c6c013a0b8ca3
Koen Verheyen <koen.verheyen@inuits.eu>
2020-12-29 15:13:41 +0100

    Test for issue 503 (https://github.ugent.be/Universiteitsbibliotheek/lludss-interface/issues/503)

---
+++ cypress/integration/issues/issue-503.spec.js
@@ -0,0 +1,14 @@
+// https://github.ugent.be/Universiteitsbibliotheek/lludss-interface/issues/503
+describe('A fix for issue 503', () => {
+  it('should not return a server error on the catalog page when the "q" query param is missing', () => {
+    cy.request({ url: '/catalog', failOnStatusCode: false })
+      .its('status')
+      .should('eq', 200)
+
+    cy.visit('/catalog')
+
+    cy.visit('/nl/catalog')
+
+    cy.visit('/en/catalog')
+  })
+})
