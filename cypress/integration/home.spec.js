264771edfb8871f2bd0574a7f0ed103e31aa642a
Koen Verheyen <koen.verheyen@inuits.eu>
2021-02-04 11:34:00 +0100

    Fixed redirect test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,12 +1,12 @@
-describe('The home page', function() {
-  it('should redirect HTTP protocol requests to HTTPS', function() {
+describe('The home page', function () {
+  it('should redirect HTTP protocol requests to HTTPS', function () {
     let baseUrl = Cypress.config('baseUrl').replace('https://', 'http://')
 
     cy.request({
       url: baseUrl,
-      followRedirect: false
+      followRedirect: false,
     }).then(resp => {
-      expect(resp.status).to.eq(302)
+      expect(resp.status).to.eq(301)
       expect(resp.redirectedToUrl).to.match(/^https:\/\//)
     })
 
@@ -15,31 +15,24 @@ describe('The home page', function() {
     cy.location('href').should('match', /^https:\/\//)
   })
 
-  describe('The search form', function() {
+  describe('The search form', function () {
     before(() => cy.visit('/'))
 
-    it('should be displayed', function() {
+    it('should be displayed', function () {
       cy.get('form')
         .should('exist')
         .should('have.attr', 'method', 'get')
         .should('have.attr', 'action', '/en/catalog')
-        .within(function($form) {
-          cy.get('label')
-            .should('have.class', 'sr-only')
-            .should('have.text', 'Search...')
+        .within(function ($form) {
+          cy.get('label').should('have.class', 'sr-only').should('have.text', 'Search...')
 
-          cy.get('input[name="q"]')
-            .should('have.attr', 'autofocus', 'autofocus')
-            .should('be.visible')
+          cy.get('input[name="q"]').should('have.attr', 'autofocus', 'autofocus').should('be.visible')
 
-          cy.get('button[type=submit]')
-            .should('be.visible')
-            .prop('innerText')
-            .should('contain', 'Search collections')
+          cy.get('button[type=submit]').should('be.visible').prop('innerText').should('contain', 'Search collections')
         })
     })
 
-    it('should post to the catalog page', function() {
+    it('should post to the catalog page', function () {
       cy.get('#q').type('Liber Floridus{enter}')
 
       cy.location('href').should('end.with', '/en/catalog?q=Liber+Floridus')
