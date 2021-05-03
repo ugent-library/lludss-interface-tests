7d182744dc30a5a4470bf4a9fa88fe01456a270b
Koen Verheyen <koen.verheyen@inuits.eu>
2021-05-03 11:07:17 +0200

    Revert "Lowered threshold for ser01 test"
    This reverts commit c322188c601033268a7959652d1c96b6286eb058.

--- cypress/integration/catalog/sources/ser01.spec.js
+++ cypress/integration/catalog/sources/ser01.spec.js
@@ -1,7 +1,7 @@
-describe('Data source ser01', function () {
-  it('should have more than 30K hits', function () {
+describe('Data source ser01', function() {
+  it('should have more than 45K hits', function() {
     cy.visit('/catalog/source:ser01')
 
-    cy.getCount().should('be.greaterThan', 30000)
+    cy.getCount().should('be.greaterThan', 45000)
   })
 })
