89a0e97e93ad8b567aba2b321e283e376116dc72
Koen Verheyen <koen.verheyen@inuits.eu>
2021-05-07 16:04:31 +0200

    Test for non-translated languages in language facet

---
+++ cypress/integration/catalog/types/language-facet.spec.js
@@ -0,0 +1,19 @@
+const paths = {
+  '/catalog': 'Language',
+  '/nl/catalog': 'Taal',
+  '/en/catalog': 'Language',
+}
+
+describe('The language facet', () => {
+  Object.keys(paths).forEach(path => {
+    it(`should not have non-translated languages for path ${path}`, () => {
+      cy.visit(path)
+
+      cy.contains('.filters .form-group', paths[path])
+        .find('.checkbox label .label')
+        .each((_, i, w) => {
+          cy.wrap(w.eq(i).text()).should('not.match', /^[a-z]{3}$/)
+        })
+    })
+  })
+})
