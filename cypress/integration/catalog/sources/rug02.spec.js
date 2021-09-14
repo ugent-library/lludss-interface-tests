0b0e5d9d4b48c854fd456d401c8c6f14195c77e9
Koen Verheyen <koen.verheyen@inuits.eu>
2021-09-14 11:17:57 +0200

    Improved rug02 test

--- cypress/integration/catalog/sources/rug02.spec.js
+++ cypress/integration/catalog/sources/rug02.spec.js
@@ -1,8 +1,7 @@
 describe('Data source rug02', function () {
-  // rug02 data should be decreasing. Test probably can be removed or improved.
-  it.skip('should have more than 500K hits', function () {
+  it('should have less than 500K hits', function () {
     cy.visit('/catalog/source:rug02')
 
-    cy.getCount().should('be.greaterThan', 500000)
+    cy.getCount().should('be.lessThan', 500000)
   })
 })
