15668d104acbfd354968b7b5915989b23b3fce54
Koen Verheyen <koen.verheyen@inuits.eu>
2022-01-10 10:45:19 +0100

    pug01 test: log Biblio total for reference

--- cypress/integration/catalog/sources/pug01.spec.js
+++ cypress/integration/catalog/sources/pug01.spec.js
@@ -1,13 +1,14 @@
-describe('Data source pug01', function() {
-  it('should have more than 230K hits', function() {
+describe('Data source pug01', function () {
+  it('should have more than 230K hits', function () {
     cy.visit('/catalog/source:pug01')
 
     cy.getCount().should('be.greaterThan', 230000)
   })
 
-  it('should have almost as many hits as biblio', function() {
+  it('should have almost as many hits as biblio', function () {
     cy.request('https://biblio.ugent.be/publication?limit=0&format=json').then(response => {
       const biblioTotal = response.body.total
+      cy.log(`Biblio has ${biblioTotal} hits`)
 
       cy.visit('/catalog/source:pug01')
 
