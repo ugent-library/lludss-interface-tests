5ee0b09b5bc75f9e9ea462fc94fa1e22d478e2f6
Koen Verheyen <koen.verheyen@inuits.eu>
2022-03-21 16:03:38 +0100

    Fixed OAI and opensearch tests for new fast-xml-parser version

--- cypress/integration/oai/oai.spec.js
+++ cypress/integration/oai/oai.spec.js
@@ -1,17 +1,19 @@
-import { validate, parse } from 'fast-xml-parser'
+import { XMLValidator, XMLParser } from 'fast-xml-parser'
 
-describe('The OAI API', function() {
+describe('The OAI API', function () {
   let verbs = {
     GetRecord: 'identifier=rug01:001365117&metadataPrefix=oai_dc',
     Identify: null,
     ListIdentifiers: 'metadataPrefix=oai_dc',
     ListMetadataFormats: null,
     ListRecords: 'metadataPrefix=oai_dc',
-    ListSets: null
+    ListSets: null,
   }
 
-  Object.keys(verbs).forEach(function(verb) {
-    it(`should be able the handle the ${verb} verb`, function() {
+  const parser = new XMLParser()
+
+  Object.keys(verbs).forEach(function (verb) {
+    it(`should be able the handle the ${verb} verb`, function () {
       let url = `/OAI?verb=${verb}`
 
       if (verbs[verb]) {
@@ -20,10 +22,10 @@ describe('The OAI API', function() {
 
       cy.request(url)
         .its('body')
-        .should(function(body) {
-          expect(validate(body)).to.be.true
+        .should(function (body) {
+          expect(XMLValidator.validate(body)).to.be.true
 
-          let oai = parse(body)
+          let oai = parser.parse(body)
           expect(oai['OAI-PMH']).to.not.have.property('error')
         })
     })
--- cypress/integration/oai/opensearch.spec.js
+++ cypress/integration/oai/opensearch.spec.js
@@ -1,20 +1,18 @@
-import { parse } from 'fast-xml-parser'
+import { XMLParser } from 'fast-xml-parser'
 
-let parseOptions = {
+const parser = new XMLParser({
   ignoreAttributes: false,
-  attributeNamePrefix: '@'
-}
+  attributeNamePrefix: '@',
+})
 
 describe('The opensearch API', () => {
   it('should produce OpenSearch description', () => {
     cy.request('/catalog/opensearch.xml')
-      .then(response => parse(response.body, parseOptions).OpenSearchDescription)
+      .then(response => parser.parse(response.body).OpenSearchDescription)
       .should(openSearch => {
         expect(openSearch).to.have.property('ShortName', 'Ghent University Library')
         expect(openSearch).to.have.property('Description', 'Ghent University Library Search')
-        expect(openSearch)
-          .to.have.property('Url')
-          .and.to.have.length(3)
+        expect(openSearch).to.have.property('Url').and.to.have.length(3)
 
         let types = Cypress._.map(openSearch.Url, '@type')
         expect(types)
@@ -27,24 +25,20 @@ describe('The opensearch API', () => {
   describe('The RSS API', () => {
     it('should produce search results', () => {
       cy.request('/catalog.rss?q=troisieme%20Belvedere')
-        .then(response => parse(response.body, parseOptions).rss)
+        .then(response => parser.parse(response.body).rss)
         .should(rss => {
           expect(rss)
             .to.have.property('channel')
             .and.to.have.property('title', 'Ghent University Library Search Results')
 
           expect(rss.channel.item).to.have.property('title', 'Troisième belvédère')
-          expect(rss.channel.item)
-            .to.have.property('link')
-            .and.to.end.with('/catalog/rug01:000489124')
+          expect(rss.channel.item).to.have.property('link').and.to.end.with('/catalog/rug01:000489124')
         })
 
       cy.request('/catalog.rss?q=liber+floridus')
-        .then(response => parse(response.body, parseOptions).rss)
+        .then(response => parser.parse(response.body).rss)
         .should(rss => {
-          expect(rss.channel.item)
-            .to.be.an('array')
-            .that.has.length(20)
+          expect(rss.channel.item).to.be.an('array').that.has.length(20)
 
           let titles = Cypress._.map(rss.channel.item, 'title')
           expect(titles.filter(i => i.toLowerCase().indexOf('liber floridus') >= 0)).to.not.be.empty
@@ -57,28 +51,20 @@ describe('The opensearch API', () => {
       cy.request('/catalog/opensearch.json?q=belvedere+troisieme')
         .its('body')
         .should(body => {
-          expect(body)
-            .to.be.an('array')
-            .and.to.have.length(2)
+          expect(body).to.be.an('array').and.to.have.length(2)
 
           expect(body[0]).to.eq('belvedere troisieme')
-          expect(body[1])
-            .to.be.an('array')
-            .and.to.have.length(1)
+          expect(body[1]).to.be.an('array').and.to.have.length(1)
           expect(body[1][0]).to.eq('Troisième belvédère')
         })
 
       cy.request('/catalog/opensearch.json?q=RTBF')
         .its('body')
         .should(body => {
-          expect(body)
-            .to.be.an('array')
-            .and.to.have.length(2)
+          expect(body).to.be.an('array').and.to.have.length(2)
 
           expect(body[0]).to.eq('RTBF')
-          expect(body[1])
-            .to.be.an('array')
-            .and.to.have.length(10)
+          expect(body[1]).to.be.an('array').and.to.have.length(10)
 
           expect(body[1].filter(i => i.toLowerCase().indexOf('rtbf') >= 0)).to.not.be.empty
         })
