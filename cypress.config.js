f34e775c6008665ed3f7e9bd9b4f96c75a5a0084
Koen Verheyen <koen.verheyen@inuits.eu>
2022-06-01 20:44:41 +0200

    Migrated to Cypress 10

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: 'npac3t',
+  video: false,
+  defaultCommandTimeout: 2000,
+  viewportWidth: 1200,
+  blockHosts: ['*google-analytics.com', '*hotjar.com'],
+  env: {
+    widgetsBaseUrl: 'http://widgets.lib.ugent.be',
+  },
+  retries: 2,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'https://lib.ugent.be',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
