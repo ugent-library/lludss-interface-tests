f4009d4ba60941a9129b78a93ee8b6b1b286e216
Koen Verheyen <koen.verheyen@inuits.eu>
2024-12-12 10:26:12 +0100

    Lowered ser01 threshold to 40K

--- cypress/e2e/catalog/sources/ser01.spec.js
+++ cypress/e2e/catalog/sources/ser01.spec.js
@@ -2,6 +2,6 @@ describe("Data source ser01", () => {
   it("should have more than 45K hits", () => {
     cy.visit("/catalog/source:ser01");
 
-    cy.getCount().should("be.greaterThan", 45000);
+    cy.getCount().should("be.greaterThan", 40_000);
   });
 });
